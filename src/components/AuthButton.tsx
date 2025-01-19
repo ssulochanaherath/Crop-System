951a6efe96bf33305107159028687830f036ca4f
sangeeth <ssulochanaherath@gmail.com>
2025-01-19 11:52:33 +0530

    Few Changes

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -4,7 +4,7 @@ export function AuthButton({ children }) {
       <div className="mt-8">
         <button
           type="submit"
-          className="w-full shadow-xl py-2.5 px-4 text-sm font-semibold tracking-wider rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none transition-all"
+          className="w-full shadow-xl py-2.5 px-4 text-sm font-semibold tracking-wider rounded-md text-white bg-gray-800 hover:bg-gray-700 focus:outline-none transition-all"
         >
           {children}
         </button>
